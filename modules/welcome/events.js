c12e2ea07534b08f9266633ab0c831a67738d4c6
rikumax25 <jorgev259@outlook.es>
2018-06-05 08:37:14 -0600

    first commit

--- modules/welcome/events.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    events:{
-        async guildMemberAdd(client,db,member){
-            member.guild.channels.find(channel => channel.name=="│main-lounge").send(`Welcome to Fandom Circle, ${member}! Have Fun`);
-        }
-    }    
-}
