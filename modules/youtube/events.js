e0fc64b2ca1b611ec23ce8440e4ff92e9eae2543
Jorge Vargas <jorgev259@outlook.es>
2018-08-20 15:54:57 -0600

    different encoding

--- modules/youtube/events.js
+++ modules/youtube/events.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded());
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 module.exports = {
